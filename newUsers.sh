e882ff74c6a005aa70db8a436cbba74d93d75ed2
Anthony <anthonycsli123@gmail.com>
2018-05-29 16:13:05 -0700

    new changes

--- newUsers.sh
+++ newUsers.sh
@@ -1,11 +1,11 @@
-#!bin/bash 
+#!/bin/bash 
 
-sudo ddcl . create /Users/username
-sudo ddcl . create /Users/username RealName “username”
-sudo ddcl . create /Users/username hint “hint”
-sudo ddcl . create /Users/username password 
-sudo ddcl . create /Users/username UniqueID 600
-sudo ddcl . create /Users/username PrimaryGroupID 80
-sudo ddcl . create /Users/username UserShell /bin/bash
-sudo ddcl . create /Users/username NFSHomeDirectory /Users/cpsit
+sudo dscl . create /Users/username
+sudo dscl . create /Users/username RealName “username”
+sudo dscl . create /Users/username hint “hint”
+sudo dscl . create /Users/username password 
+sudo dscl . create /Users/username UniqueID 600
+sudo dscl . create /Users/username PrimaryGroupID 80
+sudo dscl . create /Users/username UserShell /bin/bash
+sudo dscl . create /Users/username NFSHomeDirectory /Users/cpsit
 createhomedir -u username -c
