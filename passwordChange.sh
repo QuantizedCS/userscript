4840059c7ccb78cb3a6e43c0e08869881baa1803
anthonycsli123 <31576503+anthonycsli123@users.noreply.github.com>
2018-05-30 15:42:37 -0700

    Add files via upload

--- passwordChange.sh
+++ passwordChange.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 userName=username
-passWord=password
 newPassword=newpassword
 
-echo "$passWord" | sudo -S -s
-echo "$userName:$newPassword" | sudo chpasswd
+sudo dscl . -passwd /Users/$userName "$newPassword"
