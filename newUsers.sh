e7a7aab33baaab009423bbbc05dcf6ba68b9cd65
anthonycsli123 <31576503+anthonycsli123@users.noreply.github.com>
2018-05-30 13:10:16 -0700

    Add files via upload

--- newUsers.sh
+++ newUsers.sh
@@ -1,13 +1,18 @@
-#!/bin/bash 
+#!/bin/bash
 
-sudo dscl . create /Users/username
-sudo dscl . create /Users/username RealName “username”
-sudo dscl . create /Users/username hint “hint”
-sudo dscl . create /Users/username password 
-sudo dscl . create /Users/username UniqueID 600
-sudo dscl . create /Users/username PrimaryGroupID 80
-sudo dscl . create /Users/username UserShell /bin/bash
-sudo dscl . create /Users/username NFSHomeDirectory /Users/username
-sudo dscl . -append /Groups/admin GroupMembership "username"
-sudo dscl . -append /Groups/_appserveradm GroupMembership "username"
-sudo dscl . -append /Groups/_appserverusr GroupMembership "username"
+userName=username
+passWord=password
+realName=realname
+Hint = hint
+
+sudo dscl . create /Users/$userName
+sudo dscl . create /Users/$userName RealName "$realName"
+sudo dscl . create /Users/$userName hint "$Hint"
+sudo dscl . create /Users/$userName $passWord 
+sudo dscl . create /Users/$userName UniqueID 600
+sudo dscl . create /Users/$userName PrimaryGroupID 80
+sudo dscl . create /Users/$userName UserShell /bin/bash
+sudo dscl . create /Users/$userName NFSHomeDirectory /Users/$userName
+sudo dscl . append /Groups/admin GroupMembership "$userName"
+sudo dscl . append /Groups/_appserveradm GroupMembership "$userName"
+sudo dscl . append /Groups/_appserverusr GroupMembership "$userName"
--- passwordChange.sh
+++ passwordChange.sh
@@ -1,2 +1,8 @@
-echo "password" | sudo -S -s
-echo"username:newpassword" | sudo chpasswd
+#!/bin/bash
+
+userName=username
+passWord=password
+newPassword = newpassword
+
+echo "$passWord" | sudo -S -s
+echo "$userName:$newPassword" | sudo chpasswd
